3bde658cd768e1a5c61f9be1334601c864e150d7
Karol <kd@Karols-MacBook-Pro.local>
2017-06-20 15:47:07 +0200

    EventListComponent

--- NMWeb/src/app/app.module.ts
+++ NMWeb/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { UserListComponent } from './user-list/user-list.component';
+import { EventListComponent } from './event-list/event-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserProfileComponent,
-    UserListComponent
+    UserListComponent,
+    EventListComponent
   ],
   imports: [
     BrowserModule
