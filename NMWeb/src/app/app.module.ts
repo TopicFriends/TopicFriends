90dd89f71065037dd1db30256843025bc083a917
Anna N <anna.m.niedzialek@gmail.com>
2017-06-21 22:53:02 +0200

    Routing added. Components generated.

--- NMWeb/src/app/app.module.ts
+++ NMWeb/src/app/app.module.ts
@@ -2,19 +2,57 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { UserListComponent } from './user-list/user-list.component';
 import { EventListComponent } from './event-list/event-list.component';
+import { EventDetailsComponent } from './event-details/event-details.component';
+import { LoginComponent } from './login/login.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'login'
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'event-list',
+    component: EventListComponent
+  },
+  {
+    path: 'event-details',
+    component: EventDetailsComponent
+  },
+  {
+    path: 'people-list',
+    component: UserListComponent
+  },
+  {
+    path: 'profile',
+    component: UserProfileComponent
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     UserProfileComponent,
     UserListComponent,
-    EventListComponent
-  ],
+    EventListComponent,
+    EventDetailsComponent,
+    LoginComponent
+    ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
