e4e301c8d24f4ce76f9c6e8a45a0fa8d2b7805bd
Pablo <orti90@gmail.com>
2017-06-21 23:37:26 +0200

    Added Angular material v2.beta6

--- NMWeb/src/app/app.module.ts
+++ NMWeb/src/app/app.module.ts
@@ -1,10 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule,CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
 import { AppComponent } from './app.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { UserListComponent } from './user-list/user-list.component';
 import { EventListComponent } from './event-list/event-list.component';
+import {MdButtonModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MdSlideToggleModule} from '@angular/material';
+import { MdIconModule,  } from '@angular/material';
+
+
+
+import 'hammerjs';
 
 @NgModule({
   declarations: [
@@ -14,9 +21,14 @@ import { EventListComponent } from './event-list/event-list.component';
     EventListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdButtonModule,
+    MdSlideToggleModule,
+    MdIconModule
   ],
   providers: [],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
