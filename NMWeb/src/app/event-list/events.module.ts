0cb0e69a20ae3ba94985ffe80f5abcd327cef07d
Anna Niedzialek <anna.m.niedzialek@gmail.com>
2017-06-24 23:10:43 +0200

    Events module. Events list view update.

---
+++ NMWeb/src/app/event-list/events.module.ts
@@ -0,0 +1,35 @@
+/**
+ * Created by anna on 24/06/2017.
+ */
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+import { EventListComponent }    from './event-list.component';
+import { EventListItemComponent }  from './event-list-item/event-list-item.component';
+import { EventDetailsComponent }  from './event-details/event-details.component';
+import { MdListModule, MdCardModule, MdButtonModule } from '@angular/material';
+
+const eventRoutes: Routes = [
+  { path: 'event-list',  component: EventListComponent },
+  { path: 'event-details/:id', component: EventDetailsComponent }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MdListModule,
+    MdCardModule,
+    MdButtonModule,
+    RouterModule.forChild(eventRoutes)
+  ],
+  declarations: [
+    EventListComponent,
+    EventListItemComponent,
+    EventDetailsComponent
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class EventsModule { }
