65a43e24a7cbe4f5880d3cd90e352be918306528
Anna Niedzialek <anna.m.niedzialek@gmail.com>
2017-07-01 20:15:31 +0200

    Test - WhatUserWants, test data, test cases WIP

--- NMWeb/src/app/user-profile/what-user-wants.example-data.ts
+++ NMWeb/src/app/user-profile/what-user-wants.example-data.ts
@@ -1,74 +1,109 @@
-import {WantedTopics, WhatUserWants} from './user-profile.service';
-/**
- * Created by annab on 29/06/2017.
- */
+import {UserProfile, WantedTopics, WhatUserWants} from './user-profile.service';
+
+const topic_Angular =
+  {
+    active: true,
+    topicId: 'someForeignKey_Angular',
+    name: 'Angular',
+  };
+
+const topic_Ionic =
+  {
+    active: true,
+    topicId: 'someForeignKey_Ionic',
+    name: 'Ionic',
+  };
+
+const topic_Android =
+  {
+    active: true,
+    name: 'Android',
+    topicId: 'someForeignKey_Android',
+  };
+
+// Firebase: objects that have no children become null. Simulate that in passing data around here.
+// The null is for all the objects that have no children, so if distant descendant has no children, all ancestors don't exist.
 
 export class ExampleData {
-  // public User_1_wantsEverything: WhatUserWants = {
-  //   byInteractionMode: {
-  //     symmetric: {
-  //       exchange: {
-  //         topics: {
-  //           pushId1: {
-  //             active: true,
-  //             topicId: 'someForeignKey_Angular',
-  //             name: 'Angular',
-  //           },
-  //           pushId2: {
-  //             active: true,
-  //             topicId: 'someForeignKey_Ionic',
-  //             name: 'Ionic',
-  //           },
-  //           pushId3: {
-  //             active: true,
-  //             name: 'WordPress',
-  //             topicId: 'someForeignKey_WordPress',
-  //           },
-  //         }
-  //       },
-  //       pairProgramming: {},
-  //       play: {},
-  //     },
-  //     supplyDemand: {
-  //       intern: {},
-  //       mentor: {},
-  //       freelance: {},
-  //       job?: SupplyDemand,
-  //       advising?: SupplyDemand,
-  //       sponsorEvents?: SupplyDemand,
-  //       coFounderSpecializingIn?: SupplyDemand,
-  //       // work on hobby project together,
-  //       /** Work on open-source together; probably move to symmetric */
-  //       contributeToOpenSource?: SupplyDemand,
-  //       /** probably move to symmetric */
-  //       hackathon?: SupplyDemand,
-  //     }
-  //   }
-  // }
 
+  public createUserWith(topics: WantedTopics, supplyDemand?: boolean) { //TODO: get rid of booleans
+    var user = new UserProfile();
+    if(supplyDemand) {
+      user.whatUserWants = this.createWhatUserWantsSupplyDemand(topics);
+    }
+    else {
+      user.whatUserWants = this.createWhatUserWantsExchange(topics);
+    }
+    user.name = 'Some user name';
+    user.suername = 'Some surname';
+    user.company = 'Some company';
+    user.role = 'Some user role';
+    user.profileFacebook = 'Facebook profile url';
+    user.profileGitHub = 'GitHub profile url';
+    user.profileLinkedIn = 'LinkedIn profile url';
+    user.profileStackOverflow = 'StackOverflow profile url';
+    user.profileTwitter = 'Twitter profile url';
 
-  public User_2_wantsNothing: WhatUserWants;
+    return user;
+  }
 
-  // number: set, letter: data matching
-  public User_3a_wantsToMentorOnIonic: WhatUserWants;
+  public readonly topics_Default = {
+    topics: {
+      pushId1: topic_Angular,
+      pushId2: topic_Ionic,
+      pushId3: topic_Android,
+    }
+  };
 
-  public User_3b_wantsToBeMentoredOnIonic: WhatUserWants;
+  public readonly topics_Empty = {
+    topics: null
+  };
 
-  public User_3c_wantsToBeMentoredOnBusiness: WhatUserWants;
+  public readonly topics_Ionic = {    //User_3a_wantsToMentor_Ionic, User_3b_wantsToBeMentored_Ionic
+    topics: {
+      pushId2: topic_Ionic
+    }
+  };
 
-  public User_4a_wantsFreelancingJob: WhatUserWants;
+  public readonly topics_Android = {
+    topics: {
+      pushId2: topic_Android
+    }
+  };  //User_3c_wantsToBeMentored_Android   //replace with a b
 
-  public User_4b_wantsToHireFreelancer: WhatUserWants;
+  public readonly User_4a_wantsFreelancingJob_GraphicDesing;
 
-  public User_4c_wantsToHireFreelancer: WhatUserWants;
+  public readonly User_4b_wantsToHireFreelancer_GraphicDesing;
 
-  public User_5a_wantsAnInternship: WhatUserWants;
+  public readonly User_4c_wantsFreelancingJob_FullStack;
 
-  public User_5b_wantsToHireAnIntern: WhatUserWants;
+  public readonly User_5a_wantsAnInternship_Android;
 
-  public User_6a_wantsAJob: WhatUserWants;
+  public readonly User_5b_wantsToHireAnIntern_Android;  //split into two
 
-  public User_6b_wantsToHireForJob: WhatUserWants;
+  public readonly User_6a_wantsAJob_FullStack;
 
-  // TODO
+  public readonly User_6b_wantsToHireForAJob_FullStack;
+
+  public readonly User_6c_wantsToHireForAJob_GraphicDesign;
+
+  private createWhatUserWantsSupplyDemand(topics: WantedTopics) {
+    return   {
+      byInteractionMode: {
+        supplyDemand: {
+          freelance: {
+            supply: topics
+          }
+        }
+      }
+    }
+  }
+
+  private createWhatUserWantsExchange(topics: WantedTopics) {
+    return   {
+      byInteractionMode: {
+        exchange: topics
+      }
+    }
+  }
 }
