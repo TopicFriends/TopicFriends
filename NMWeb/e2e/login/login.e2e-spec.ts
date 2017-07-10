77f90e72084c11d10ee8c595f977744fdc2f14db
Anna Niedzialek <anna.m.niedzialek@gmail.com>
2017-07-10 16:46:37 +0200

    Test - logging in with google

--- NMWeb/e2e/login/login.e2e-spec.ts
+++ NMWeb/e2e/login/login.e2e-spec.ts
@@ -1,18 +1,22 @@
 import { Login } from './login.po';
 import {browser} from 'protractor';
 // import { AngularFireAuth } from 'angularfire2/auth';
+browser.waitForAngularEnabled(false);
 
 fdescribe('User', () => {
   let page: Login;
 
-  beforeEach(() => {
+  beforeAll(() => {
     page = new Login();
   });
 
   it('should be able to log in', () => {
     page.navigateTo();
-    browser.sleep(20000).then(() => {
-    }
-    );// user info is inside auth object);
+    var success = page.signInDefaultTestUser();
+    expect(success).toBeTruthy();
+  });
+
+  it('name should be displayed', () => {    //should be fetched from google
+
   });
 });
--- NMWeb/e2e/login/login.po.ts
+++ NMWeb/e2e/login/login.po.ts
@@ -1,19 +1,51 @@
-import { browser } from 'protractor';
+import {$, browser, by, element, ExpectedConditions} from 'protractor';
 import * as environment from '../../src/environments/environment.qa';
 
 var firebase = require("firebase");
 require("firebase/auth");
 
 export class Login {
+  defaultleep = 2000;
   userEmail = 'peoplematchertest@gmail.com';
   userPassword = '@ngul@rAppT3st!ng';
+  loginButton = $('md-toolbar button');
+  usernameField = $('#identifierId');
+  passwordField = $('#password input');
+  googleIdNextButton = $('#identifierNext');
+  googlePasswordNextButton = $('#passwordNext');
 
   navigateTo() {
     browser.get('/');
   }
 
   signInDefaultTestUser() {
+    this.loginButton.click();
 
+    browser.getAllWindowHandles().then(function (handles) {
+      browser.driver.switchTo().window(handles[1]);
+    });
+
+    browser.wait(ExpectedConditions.presenceOf(this.usernameField));
+    this.usernameField.sendKeys(this.userEmail);
+    browser.wait(ExpectedConditions.presenceOf(this.googleIdNextButton));
+    browser.sleep(this.defaultleep);
+    this.googleIdNextButton.click();
+    
+    browser.sleep(this.defaultleep);
+    browser.wait(ExpectedConditions.presenceOf(this.passwordField));
+    this.passwordField.sendKeys(this.userPassword);
+    browser.wait(ExpectedConditions.presenceOf(this.googlePasswordNextButton));
+    this.googlePasswordNextButton.click();
+
+    browser.getAllWindowHandles().then(function (handles) {
+      browser.driver.switchTo().window(handles[0]);
+    });
+
+    return this.confirmUserLoggedIn();
+  }
+
+  private confirmUserLoggedIn(): any {
+    return element(by.cssContainingText('app-login > p', 'login works!')).isPresent();
   }
 
   logoutUser() {
@@ -21,24 +53,24 @@ export class Login {
     // confirmUserLoggedOut(userId);
   }
 
-  confirmUserLoggedOut(userId) {
+  private confirmUserLoggedOut(userId) {
 
   }
 
   // CLEANUP FOR LOGIN TESTS
-  getCurrentUserUidFromFirebase() {
-
-    firebase.initializeApp(environment.environment.firebase);
-    let fireBaseUser = firebase.auth().currentUser;
-    console.log(fireBaseUser)
-    firebase.auth().onAuthStateChanged((user) => {
-      if (user) {
-        console.log(user)
-      } else {
-        console.log('no');
-      }
-    });
-  }
+  // getCurrentUserUidFromFirebase() {
+  //
+  //   firebase.initializeApp(environment.environment.firebase);
+  //   let fireBaseUser = firebase.auth().currentUser;
+  //   console.log(fireBaseUser)
+  //   firebase.auth().onAuthStateChanged((user) => {
+  //     if (user) {
+  //       console.log(user)
+  //     } else {
+  //       console.log('no');
+  //     }
+  //   });
+  // }
 /*var p = new Promise(function(resolve, reject) {
  var observer = function(user) {
  // Unsubscribe on first call.
