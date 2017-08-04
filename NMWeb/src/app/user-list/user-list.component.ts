2d36cac41916c62542ee03d108aed6be2483bb08
Karol Depka Pradzinski <karol-depka@users.noreply.github.com>
2017-08-05 00:51:43 +0200

    user-list: trackBy

--- NMWeb/src/app/user-list/user-list.component.ts
+++ NMWeb/src/app/user-list/user-list.component.ts
@@ -21,4 +21,8 @@ export class UserListComponent implements OnInit {
     this.userList = this.userListService.listUserDataWithDetails();
   }
 
+  trackByKey(idx, val) {
+    return val.$key
+  }
+
 }
