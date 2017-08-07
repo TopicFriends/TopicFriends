29ca43395a66c3da09e31e0b2829aa0614fa0a32
Karol Depka Pradzinski <karol-depka@users.noreply.github.com>
2017-08-08 00:53:26 +0200

    Fixed bug which was causing not keeping unmodified topic groups (because needed @Output fired)

--- NMWeb/src/app/user-profile/user-interests/user-interests.component.ts
+++ NMWeb/src/app/user-profile/user-interests/user-interests.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {SymmetricInteractions, TopicInterest, UserInterests} from '../user-interests'
 import {Observable} from 'rxjs/Observable'
 import {createTopicsDictionary, UserProfileService} from '../user-profile.service'
 import {AuthService} from '../auth.service'
+import {ItemListInputComponent} from '../item-list-input/item-list-input.component'
 
 @Component({
   selector: 'app-user-interests',
@@ -13,7 +14,11 @@ export class UserInterestsComponent implements OnInit {
 
   whatUserWants = UserInterests.fromJson({});
 
-  symmetricInteractions = new SymmetricInteractions();
+  @ViewChild('topicsExchangeComponent') topicsExchangeComponent: ItemListInputComponent;
+  @ViewChild('topicsHackathonComponent') topicsHackathonComponent: ItemListInputComponent;
+  @ViewChild('topicsPairProgrammingComponent') topicsPairProgrammingComponent: ItemListInputComponent;
+
+  // symmetricInteractions = new SymmetricInteractions();
   userInterestsObservable: Observable<UserInterests>;
   userInterests: UserInterests
 
@@ -37,33 +42,44 @@ export class UserInterestsComponent implements OnInit {
   }
 
   updateWantExchange(event: {tagList: TopicInterest[]}) {
+    // // console.log('updateWantExchange', event);
     // console.log('updateWantExchange', event);
-    console.log('updateWantExchange', event);
-    console.log('updateWantExchange', this.whatUserWants);
-    // this.whatUserWants.byInteractionMode.symmetric.exchange.topics = {};
-    this.symmetricInteractions.exchange = {
-      topics: createTopicsDictionary(event.tagList),
-    };
+    // console.log('updateWantExchange', this.whatUserWants);
+    // // this.whatUserWants.byInteractionMode.symmetric.exchange.topics = {};
+    // this.symmetricInteractions.exchange = {
+    //   topics: createTopicsDictionary(event.tagList),
+    // };
 
   }
 
   updateWantHackathon(event: {tagList: TopicInterest[]}) {
-    this.symmetricInteractions.hackathon = {
-      topics: createTopicsDictionary(event.tagList),
-    };
+    // this.symmetricInteractions.hackathon = {
+    //   topics: createTopicsDictionary(event.tagList),
+    // };
   }
 
   updateWantPairProgramming(event: {tagList: TopicInterest[]}) {
-    this.symmetricInteractions.pairProgramming = {
-      topics: createTopicsDictionary(event.tagList),
-    };
+    // this.symmetricInteractions.pairProgramming = {
+    //   topics: createTopicsDictionary(event.tagList),
+    // };
   }
 
 
   getUserInterests() {
+    const symmetricInteractions = new SymmetricInteractions();
+    symmetricInteractions.exchange = {
+      topics: createTopicsDictionary(this.topicsExchangeComponent.tagListModel.tags),
+    };
+    symmetricInteractions.hackathon = {
+      topics: createTopicsDictionary(this.topicsHackathonComponent.tagListModel.tags),
+    };
+    symmetricInteractions.pairProgramming = {
+      topics: createTopicsDictionary(this.topicsPairProgrammingComponent.tagListModel.tags),
+    };
+
     return UserInterests.fromJson({
       byInteractionMode: {
-        symmetric: this.symmetricInteractions,
+        symmetric: symmetricInteractions,
       }
     })
   }
