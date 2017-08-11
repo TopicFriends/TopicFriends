d5eed782f40a7ae032e0eefb5e6038bb2dbaa9db
Karol Depka Pradzinski <karol-depka@users.noreply.github.com>
2017-08-11 23:41:01 +0200

    SnackBar

--- NMWeb/src/app/shared/shared.module.ts
+++ NMWeb/src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {
   MdButtonModule, MdCheckboxModule, MdChipList, MdChipsModule, MdIconModule, MdInputModule, MdMenuModule,
-  MdSlideToggleModule,
+  MdSlideToggleModule, MdSnackBar, MdSnackBarModule,
 } from '@angular/material'
 import {TopicTagComponent} from './topic-tag/topic-tag.component'
 import {TopicTagListComponent} from './topic-tag-list/topic-tag-list.component'
+import {SnackBarComponent} from './snackbar/snackbar.component'
 
 @NgModule({
   imports: [
@@ -18,14 +19,20 @@ import {TopicTagListComponent} from './topic-tag-list/topic-tag-list.component'
     MdIconModule,
     MdChipsModule,
     MdIconModule,
+    MdSnackBarModule,
   ],
   declarations: [
     TopicTagComponent,
     TopicTagListComponent,
+    SnackBarComponent,
   ],
   exports: [
     TopicTagComponent,
     TopicTagListComponent,
+    SnackBarComponent,
   ],
+  providers: [
+    SnackBarComponent,
+  ]
 })
 export class SharedModule { }
