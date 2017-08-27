388e081c41ee440c3e8211cc2e4c6b0ac279ed16
Anna Niedzialek <anna.m.niedzialek@gmail.com>
2017-08-27 21:32:52 +0200

    After merge cleanup

--- NMWeb/src/app/shared/shared.module.ts
+++ NMWeb/src/app/shared/shared.module.ts
@@ -4,12 +4,13 @@ import {
   MdButtonModule, MdCardModule, MdCheckboxModule, MdChipsModule,
   MdIconModule, MdInputModule,
   MdMenuModule,
-  MdSlideToggleModule,
+  MdSlideToggleModule, MdSnackBar, MdSnackBarModule,
 } from '@angular/material'
 import {TopicTagComponent} from './topic-tag/topic-tag.component'
 import {TopicTagListComponent} from './topic-tag-list/topic-tag-list.component'
 import {UserTemplateComponent} from '../user-list/user-template/user-template.component';
 import {UserInterestsModeViewComponent} from '../user-list/user-template/user-interests-mode-view/user-interests-mode-view.component';
+import {SnackBarComponent} from './snackbar/snackbar.component';
 
 @NgModule({
   imports: [
@@ -21,19 +22,26 @@ import {UserInterestsModeViewComponent} from '../user-list/user-template/user-in
     MdCheckboxModule,
     MdIconModule,
     MdChipsModule,
-    MdCardModule
+    MdCardModule,
+    MdSnackBarModule
   ],
   declarations: [
     TopicTagComponent,
     TopicTagListComponent,
     UserTemplateComponent,
-    UserInterestsModeViewComponent
+    UserInterestsModeViewComponent,
+    SnackBarComponent
   ],
   exports: [
     TopicTagComponent,
     TopicTagListComponent,
     UserTemplateComponent,
-    UserInterestsModeViewComponent
+    UserInterestsModeViewComponent,
+    SnackBarComponent
   ],
+  providers: [
+    SnackBarComponent,
+    MdSnackBar
+  ]
 })
 export class SharedModule { }
