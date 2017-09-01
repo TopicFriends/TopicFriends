b909234279c47deeb7b20e562df721eb12fc9424
Karol Depka Pradzinski <karol-depka@users.noreply.github.com>
2017-09-01 19:02:26 +0200

    Added note comments about Topic metadata like urls

--- NMWeb/src/app/user-profile/tag-entry.ts
+++ NMWeb/src/app/user-profile/tag-entry.ts
@@ -8,6 +8,13 @@ export class TagEntry {
   logo: string;
   id: string
 
+  // TODO: introduce a separate TopicMetaData or TopicPages class. Will be easier to put it in a separate firebase location.
+  // TODO: website
+  // TODO: npm page
+  // TODO: github page
+  // TODO: twitter page/handle (could be useful to filter/auto-generate News)
+  // TODO: stackSharePage, e.g. https://stackshare.io/angular-2
+
   constructor(
     public name: string,
     // public topicId?,
