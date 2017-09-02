0aebb06c7cce04f5e3fd36ef5ab6581135feb7b6
Karol Depka Pradzinski <karol-depka@users.noreply.github.com>
2017-09-02 16:27:08 +0200

    ItemListInputComponent: code cleanup

--- NMWeb/src/app/user-profile/item-list-input/item-list-input.component.ts
+++ NMWeb/src/app/user-profile/item-list-input/item-list-input.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit, Output,  EventEmitter } from '@angular/core';
-import {ControlValueAccessor, FormControl, FormGroup} from '@angular/forms';
+import {FormControl, FormGroup} from '@angular/forms';
 import { TagEntry } from "app/user-profile/tag-entry";
 import { Observable } from "rxjs/Observable";
 import 'rxjs/add/operator/startWith';
@@ -17,38 +17,7 @@ import {Subject} from 'rxjs/Subject'
   templateUrl: './item-list-input.component.html',
   styleUrls: ['./item-list-input.component.scss'],
 })
-export class ItemListInputComponent implements OnInit
-  // , ControlValueAccessor
-{
-  //reusableControl = new FormControl();
-
-  // writeValue(obj: any): void {
-  //   throw new Error("Method not implemented.");
-  // }
-  //
-  // registerOnChange(fn: any): void {
-  //   throw new Error("Method not implemented.");
-  // }
-  //
-  // registerOnTouched(fn: any): void {
-  //   throw new Error("Method not implemented.");
-  // }
-  //
-  // setDisabledState(isDisabled: boolean): void {
-  //   throw new Error("Method not implemented.");
-  // }
-
-  exampleTags = [
-    new TopicInterest(new TagEntry('Ionic')),
-    new TopicInterest(new TagEntry('UAP', null)),
-    new TopicInterest(new TagEntry('PeopleMatcher')),
-    new TopicInterest(new TagEntry('Angular')),
-    new TopicInterest(new TagEntry('Karma')),
-    new TopicInterest(new TagEntry('Protractor')),
-    new TopicInterest(new TagEntry('Ember')),
-    new TopicInterest(new TagEntry('Elm')),
-    new TopicInterest(new TagEntry('Firebase')),
-  ]
+export class ItemListInputComponent implements OnInit {
 
   /** rename: all *possible* tags */
   @Input() public inputTagList: TagEntry[]
