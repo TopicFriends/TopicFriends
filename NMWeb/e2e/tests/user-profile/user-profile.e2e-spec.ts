9a50e7f0fd5a72d81be84f0750498b3e76fdb8b8
Anna Niedzialek <anna.m.niedzialek@gmail.com>
2017-09-20 01:34:14 +0200

    Test - small update for new feature

--- NMWeb/e2e/tests/user-profile/user-profile.e2e-spec.ts
+++ NMWeb/e2e/tests/user-profile/user-profile.e2e-spec.ts
@@ -3,12 +3,14 @@ import {TestCleanUp} from '../../test-support/clean-up'
 import {TestAssertions} from '../../test-support/assertions'
 import {LoginPage} from '../login/login.po'
 import {ProtractorWrapper} from '../../test-support/protractor-wrapper'
+import {TestSupport} from '../../test-support/test-support'
 
 describe('Profile page: User', () => {
   let page: UserProfilePage
   let loginPage: LoginPage
   let cleanUp: TestCleanUp
   let assert: TestAssertions
+  let support: TestSupport
   let ptor: ProtractorWrapper
 
   beforeAll(() => {
@@ -16,11 +18,13 @@ describe('Profile page: User', () => {
     loginPage = new LoginPage()
     cleanUp = new TestCleanUp()
     assert = new TestAssertions()
+    support = new TestSupport()
     ptor = new ProtractorWrapper()
   });
 
   it('can see button with text "Please log in, to access your profile" when not logged in', () => {
     page.navigateTo().then(() => {
+      support.acceptAlertIfAppears()
       assert.elementIsContainingText(page.pleaseLogInButton, 'Please log in, to access your profile')
       expect(page.userProfileBasicInfo.isPresent()).toBeFalsy();   //TODO
     });
