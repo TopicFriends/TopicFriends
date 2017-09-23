ac15464f99b72b08e26f01dd935ec54a8ad647e0
David <davidbg1992@hotmail.com>
2017-09-23 16:03:01 +0200

    Fixed Facebook button on AboutPage.

--- NMWeb/src/app/about/about.component.ts
+++ NMWeb/src/app/about/about.component.ts
@@ -34,6 +34,18 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.facebookConection();
   }
 
+  facebookConection() {
+    let doc = document;
+    let script : string = 'script';
+    let sdkId : string = 'facebook-jssdk';
+
+    var js, fjs = document.getElementsByTagName(script)[0];
+    if (document.getElementById(sdkId)) return;
+    js = document.createElement(script); js.id = sdkId;
+    js.src = "//connect.facebook.net/es_ES/sdk.js#xfbml=1&version=v2.10";
+    fjs.parentNode.insertBefore(js, fjs);
+  }
 }
