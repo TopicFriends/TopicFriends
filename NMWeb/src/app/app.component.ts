5d0c7b0a91a92a40c6b4c1a52a3a0a3159426a5d
Karol Depka Pradzinski <karol-depka@users.noreply.github.com>
2017-10-20 17:49:33 +0200

    Cookie law banner: button and text

--- NMWeb/src/app/app.component.ts
+++ NMWeb/src/app/app.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild, ViewEncapsulation} from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import { AngularFireAuth } from 'angularfire2/auth';
 import {AuthService} from './user-profile/auth.service';
+import {CookieLawComponent} from 'angular2-cookie-law'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None /* to adjust things inside cookie law banner */,
 })
 export class AppComponent {
 
+  @ViewChild(CookieLawComponent) cookieLawComponent: CookieLawComponent
+
+  dismissCookieLaw() {
+    this.cookieLawComponent.dismiss()
+  }
 
 }
