4d8f9cfe9276b55806643f76bbabfc161de95030
Antonio David Ponce Martínez <zolastrodavid@gmail.com>
2018-03-01 16:01:20 +0100

    Going through all users with the same topic

--- NMWeb/src/app/topics/topic-details/topic-details.component.ts
+++ NMWeb/src/app/topics/topic-details/topic-details.component.ts
@@ -7,7 +7,8 @@ import {TopicInterest} from '../../user-profile/user-interests'
 import {GitHubService} from '../../shared/git-hub.service'
 import {Title} from "@angular/platform-browser";
 import { GeoLocationService } from '../../shared/geo-location.service';
-import { GeoLocation } from '../../user-profile/user-profile.service';
+import { GeoLocation, GeoLocationsDictionary } from '../../user-profile/user-profile.service';
+import { UserMatcherService } from '../../user-matcher.service';
 
 @Component({
   selector: 'app-topic-details',
@@ -20,13 +21,14 @@ export class TopicDetailsComponent implements OnInit {
   coordinates: GeoLocation = {latitude: 36.726, longitude: -4.476} /* mock default value for faster testing */;
   topic: TagEntry
   topicInterest: TopicInterest
-
+  userCoordinates: GeoLocationsDictionary[];
   constructor(
     private route: ActivatedRoute,
     private topicsService: TopicsService,
     private gitHubService: GitHubService,
     private titleService: Title,
-    private geoLocationService: GeoLocationService
+    private geoLocationService: GeoLocationService, 
+    private userMatcherService: UserMatcherService
   ) {
     this.topic = this.topicsService.getTopicById(this.topicId)
     this.topicInterest = this.createTopicInterest(this.topic);
@@ -42,6 +44,26 @@ export class TopicDetailsComponent implements OnInit {
         // };
       }
     );
+
+    this.userMatcherService.listUsersSortedByLastModified().subscribe(users => {
+      users.map(user => {
+        if(user.userDataCombined.interests.byInteractionMode) {
+          let topics = user.userDataCombined.interests.byInteractionMode.symmetric.exchange.topics;
+          // console.log(topics);
+          if(topics) {
+            let i = 0;
+            while(topics[i]) {
+              if(topics[i].tagEntry.id == this.topic.id){
+                this.userCoordinates.push(user.userDataCombined.geoLocations.geoLocations.whereILive);
+                console.log(topics[i]);
+              }
+              
+              i++;
+            }
+          }          
+        }
+      });
+    });
   }
 
   createTopicInterest(topic) {
