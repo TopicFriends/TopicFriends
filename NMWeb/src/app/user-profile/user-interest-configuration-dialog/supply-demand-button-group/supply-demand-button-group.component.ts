7436d4d10c64e0c434043bf4d66ac7d64d7a18fe
mjimenezmartin <jimenezmartinmoises@gmail.com>
2018-03-04 12:31:26 +0100

    using interface for data declaration

--- NMWeb/src/app/user-profile/user-interest-configuration-dialog/supply-demand-button-group/supply-demand-button-group.component.ts
+++ NMWeb/src/app/user-profile/user-interest-configuration-dialog/supply-demand-button-group/supply-demand-button-group.component.ts
@@ -1,13 +1,19 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+interface supplyDemandAction {
+  title: string,
+  actions: string[]
+};
+
 @Component({
   selector: 'app-supply-demand-button-group',
   templateUrl: './supply-demand-button-group.component.html',
   styleUrls: ['./supply-demand-button-group.component.scss']
 })
+
 export class SupplyDemandButtonGroupComponent implements OnInit {
 
-  @Input() public data:{};
+  @Input() public data:supplyDemandAction;
 
   constructor() { }
 
