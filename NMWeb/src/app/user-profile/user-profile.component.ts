2a9e6477c5d79e6669e902fa07005641bd91ddc3
Karol Depka Pradzinski <karol-depka@users.noreply.github.com>
2018-03-04 17:52:01 +0100

    Remove `this.checkPrivacityValue` from `hasUnsavedChanges()`

--- NMWeb/src/app/user-profile/user-profile.component.ts
+++ NMWeb/src/app/user-profile/user-profile.component.ts
@@ -172,7 +172,7 @@ export class UserProfileComponent implements OnInit {
   hasUnsavedChanges() {
     // console.log('hasUnsavedChanges, userProfileInputs:', this.userProfileInputs)
     // return false
-    return this.userProfileInputs && this.userProfileInputs.isEditable && this.formGroup.dirty && this.checkPrivacityValue
+    return this.userProfileInputs && this.userProfileInputs.isEditable && this.formGroup.dirty
   }
 
 }
