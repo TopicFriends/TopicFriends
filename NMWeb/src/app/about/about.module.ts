a4846bf1d63fd6e4fd109b90147c8a9c82b63ab6
Karol Depka Pradzinski <karol-depka@users.noreply.github.com>
2018-03-14 16:43:05 +0100

    Fix AboutModule imports

--- NMWeb/src/app/about/about.module.ts
+++ NMWeb/src/app/about/about.module.ts
@@ -4,12 +4,16 @@ import { CommonModule } from '@angular/common';
 import { AboutRoutingModule } from './about-routing.module';
 import { SharedModule } from '../shared/shared.module'
 import { AboutComponent } from './about.component'
+import { TopicsSharedModule } from '../topics-shared/topics-shared.module'
+import { MatChipsModule } from '@angular/material'
 
 @NgModule({
   imports: [
     CommonModule,
     AboutRoutingModule,
     SharedModule,
+    TopicsSharedModule,
+    MatChipsModule /* later will have a dedicated topics-list-inline component instead of relying on mat chips */,
   ],
   declarations: [
     AboutComponent,
