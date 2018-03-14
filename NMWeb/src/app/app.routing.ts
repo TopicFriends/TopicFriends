0188f1b877bca77a09bf17fdc9cedd959b6e72d1
Karol Depka Pradzinski <karol-depka@users.noreply.github.com>
2018-03-14 15:42:56 +0100

    Remove unnecessary imports after lazy loading.

--- NMWeb/src/app/app.routing.ts
+++ NMWeb/src/app/app.routing.ts
@@ -1,16 +1,12 @@
 import { Routes, RouterModule } from '@angular/router';
 import { PreloadAllModules } from '@angular/router';
 import {AboutComponent} from './about/about.component'
-import {LoginComponent} from './login/login.component'
 import {UserGroupsComponent} from './user-groups/user-groups.component'
 import {TopicsListComponent} from './topics-list/topics-list.component'
 import {TermsOfServiceComponent} from './terms-of-service/terms-of-service.component'
 import {PrivacyPolicyComponent} from './privacy-policy/privacy-policy.component'
 import {UserListComponent} from './user-list/user-list.component'
 import {TopicsMapPageComponent} from './topics-map-page/topics-map-page.component'
-import { UserProfileDetailsComponent } from './user-profile-details/user-profile-details.component'
-
-export const DASHBOARD_ROUTE = 'dashboard'
 
 
 const appRoutes: Routes = [
