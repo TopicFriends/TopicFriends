8bb5e1b09dafdc9cba08772ffe5bb925a6728713
David Ponce <zolastrodavid@gmail.com>
2018-03-20 23:55:08 +0100

    UserPickLocationComponent now uses the PrimeNg Dialog

--- NMWeb/src/app/map-location-picker/map-location-picker.module.ts
+++ NMWeb/src/app/map-location-picker/map-location-picker.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MapsSharedModule} from '../maps-shared/maps-shared.module'
+import {DialogModule} from 'primeng/dialog'
+import {UserPickLocationComponent} from './user-pick-location/user-pick-location.component'
 
 @NgModule({
   imports: [
     CommonModule,
     MapsSharedModule,
+    DialogModule,
   ],
-  declarations: []
+  declarations: [],
+  exports: [
+    UserPickLocationComponent
+  ],
 })
 export class MapLocationPickerModule { }
