0d42bf39fc2112e727ed6f31cbb3b3ba4b28ba51
David Ponce <zolastrodavid@gmail.com>
2018-03-24 20:36:59 +0100

    Sort interested users (in topic details)
    Closes #89

--- NMWeb/src/app/topic-details/topic-details.component.ts
+++ NMWeb/src/app/topic-details/topic-details.component.ts
@@ -48,7 +48,7 @@ export class TopicDetailsComponent implements OnInit {
   ngOnInit() {
     this.titleService.setTitle( this.topic.name + ' - TopicFriends');
     this.topicsDetailsService.getMatchedUsersWithTopic(this.topicId).subscribe((users) => {
-      this.interestedUsers = users;
+      this.interestedUsers = users.sort(UserMatcherService.sortByScore);
     });
   }
 
--- NMWeb/src/app/user-profile-shared/user-matcher.service.ts
+++ NMWeb/src/app/user-profile-shared/user-matcher.service.ts
@@ -110,6 +110,12 @@ export class UserMatcherService {
     })
   }
 
-
+  static sortByScore(userA: UserMatched, userB: UserMatched) {
+    if(!userA.matchResults) {
+      return 0;
+    } else {
+      return userB.matchResults.matchScore - userA.matchResults.matchScore;
+    }
+  }
 
 }
