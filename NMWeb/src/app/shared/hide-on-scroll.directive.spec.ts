e6b0066488a5bffe3de794cd5462b67aa8f5179f
David Ponce <zolastrodavid@gmail.com>
2018-03-24 21:48:55 +0100

    HideOnScroll directive

---
+++ NMWeb/src/app/shared/hide-on-scroll.directive.spec.ts
@@ -0,0 +1,8 @@
+import { HideOnScrollDirective } from './hide-on-scroll.directive';
+
+describe('HideOnScrollDirective', () => {
+  it('should create an instance', () => {
+    const directive = new HideOnScrollDirective();
+    expect(directive).toBeTruthy();
+  });
+});
