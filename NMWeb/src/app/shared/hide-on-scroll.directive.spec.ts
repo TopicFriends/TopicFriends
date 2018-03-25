0f494ff6efe0a389cedf3487316665837d8016f9
David Ponce <zolastrodavid@gmail.com>
2018-03-25 22:16:56 +0200

    Removed failing test

--- NMWeb/src/app/shared/hide-on-scroll.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { HideOnScrollDirective } from './hide-on-scroll.directive';
-
-describe('HideOnScrollDirective', () => {
-  it('should create an instance', () => {
-    const directive = new HideOnScrollDirective();
-    expect(directive).toBeTruthy();
-  });
-});
