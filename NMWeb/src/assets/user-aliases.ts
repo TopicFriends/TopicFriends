c05d26c40c57e892ec29eaf1c458bb290dfba890
Anna <anna.m.niedzialek@gmail.com>
2018-04-10 22:24:11 +0200

    Adding my alias

--- NMWeb/src/assets/user-aliases.ts
+++ NMWeb/src/assets/user-aliases.ts
@@ -2,5 +2,6 @@ export const userAliases = {
   "david_ponce": "O4rFNstIVvXi0JRdwJ2JjFHxFk73",
   "karol_depka": "Q6RdZXc1nJbVPe3zAkIhtgXG9n32",
   "luis_podadera": "NrgIBYeii1Ms1eqBK6pkjqKaSkJ3",
-  "alejandro_mornie": "nx3uayQBU3e3rBJkx6YU7HJZCxf1"
+  "alejandro_mornie": "nx3uayQBU3e3rBJkx6YU7HJZCxf1",
+  "anna_niedzialek": "QXfSwwOATxcc2w47LKHsa8q9DxL2"
 }
