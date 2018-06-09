b48a90d1802c6a9f234cd8a63e65fc21f6635d3a
Karol Depka Pradzinski <karol-depka@users.noreply.github.com>
2018-06-09 20:00:23 +0200

    Add "karoldepka" user alias
    # Conflicts:
    #	NMWeb/src/assets/user-aliases.ts

--- NMWeb/src/assets/user-aliases.ts
+++ NMWeb/src/assets/user-aliases.ts
@@ -1,12 +1,19 @@
 export const userAliases = {
+
   "david_ponce": "O4rFNstIVvXi0JRdwJ2JjFHxFk73",
   "david-ponce": "O4rFNstIVvXi0JRdwJ2JjFHxFk73",
+
   "karol_depka": "Q6RdZXc1nJbVPe3zAkIhtgXG9n32",
   "karol-depka": "Q6RdZXc1nJbVPe3zAkIhtgXG9n32",
+  "karoldepka" : "Q6RdZXc1nJbVPe3zAkIhtgXG9n32",
+
   "luis_podadera": "NrgIBYeii1Ms1eqBK6pkjqKaSkJ3",
   "luis-podadera": "NrgIBYeii1Ms1eqBK6pkjqKaSkJ3",
+
   "alejandro_mornie": "nx3uayQBU3e3rBJkx6YU7HJZCxf1",
   "alejandro-mornie": "nx3uayQBU3e3rBJkx6YU7HJZCxf1",
+
   "anna_niedzialek": "QXfSwwOATxcc2w47LKHsa8q9DxL2",
   "anna-niedzialek": "QXfSwwOATxcc2w47LKHsa8q9DxL2",
+
 }
