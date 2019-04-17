5fd533f2362c5d1400425e2a6f34f0262317553d
adampi <adam.piskorek.pl@gmail.com>
2019-04-17 23:45:44 +0200

    firefox

--- NMWeb/e2e-testcafe/index.ts
+++ NMWeb/e2e-testcafe/index.ts
@@ -12,19 +12,26 @@ const googleLogIn = Selector(
 
 const userClass = Selector(".riddkc");
 //
+const usernameField = Selector("#identifierId");
+const userEmail = "qa.cod3r@gmail.com";
+
+const passwordField = Selector(
+  ".I0VJ4d > div:nth-child(1) > input:nth-child(1)"
+);
+const userPassword = "lekcjaonlineprod";
 
 fixture`TopicFriend`.page(url);
 
 test(`As a user I want to log in.`, async t => {
   await t.click(loginButton).click(googleLogIn);
   await t
-    .click(userClass)
-    .typeText("#Email", "MY_GOOGLE_EMAIL")
+
+    .typeText(usernameField, userEmail)
     .pressKey("enter")
-    .expect(Selector("#Passwd").focused)
-    .ok()
-    .typeText("#Passwd", "MY_GOOGLE_PASSWORD")
+
+    .typeText(passwordField, userPassword)
     .pressKey("enter")
-    .expect(Selector(".username-text").textContent)
-    .eql("amoskovkin");
+    .typeText("#mat-input-1", "My name")
+    .typeText("#mat-input-2", "My name")
+    .typeText("#mat-input-3", "My name");
 });
