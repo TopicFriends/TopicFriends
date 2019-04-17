9e9dfbdbb6a65cd24bb434cd9156256dde577e2c
Karol Depka Pradzinski <karol-depka@users.noreply.github.com>
2019-04-18 00:55:22 +0200

    Modify testcafe test to log in via email&password + add login button id-s

--- NMWeb/e2e-testcafe/index.ts
+++ NMWeb/e2e-testcafe/index.ts
@@ -2,8 +2,12 @@ import { Selector } from "testcafe";
 
 const url = "http://localhost:4444/";
 
-const loginButton = Selector(
-  "body > app-root > div > app-header > mat-toolbar > button.user-profile-corner-button.mat-button.ng-star-inserted > span"
+const loginOrSignupButton = Selector(
+  "#login-or-sign-up-corner-button"
+);
+
+const loginViaEmailPassword = Selector(
+  "#loginViaEmailPassword"
 );
 
 const googleLogIn = Selector(
@@ -23,15 +27,15 @@ const userPassword = "lekcjaonlineprod";
 fixture`TopicFriend`.page(url);
 
 test(`As a user I want to log in.`, async t => {
-  await t.click(loginButton).click(googleLogIn);
-  await t
-
-    .typeText(usernameField, userEmail)
-    .pressKey("enter")
-
-    .typeText(passwordField, userPassword)
-    .pressKey("enter")
-    .typeText("#mat-input-1", "My name")
-    .typeText("#mat-input-2", "My name")
-    .typeText("#mat-input-3", "My name");
+  await t.click(loginOrSignupButton).click(loginViaEmailPassword);
+  // await t
+  //   .typeText(usernameField, userEmail)
+  //   .click()
+    // .pressKey("enter")
+    //
+    // .typeText(passwordField, userPassword)
+    // .pressKey("enter")
+    // .typeText("#mat-input-1", "My name")
+    // .typeText("#mat-input-2", "My name")
+    // .typeText("#mat-input-3", "My name");
 });
