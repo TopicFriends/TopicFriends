3e4f5725144dc0eaf3400ed18b39b0dba07980c6
Karol <karol-depka@users.noreply.github.com>
2019-05-08 19:31:50 +0200

    Add testcafe test to navigate to all pages without reloading between

--- NMWeb/e2e-testcafe/testSrc/index.ts
+++ NMWeb/e2e-testcafe/testSrc/index.ts
@@ -8,51 +8,15 @@ import {
 } from "./login/login.testc";
 import { userProfileTest } from "./profile/userProfile.testc";
 import { LOCALHOST_URL } from "./utilsGlobal/globals";
+import { navToPagesTests } from './navigation.testc'
 
 (fixture`TopicFriends` as any)
   // .disablePageReloads
   .page(LOCALHOST_URL);
 
-userProfileTest();
+// NOTE: Put fastest tests first
+
 loginViaProfileTest();
 loginTest();
-/** DRY CoC */
-export async function navTo(t, name, { openHamburger }) {
-  // await tz
-  // .maximizeWindow( )
-  console.log("navTo", name);
-  if (openHamburger) {
-    await t.click("#menuButtonHamburger");
-  }
-  await t
-    .click("#navTo" + name)
-    .expect(getLocation())
-    .contains("/" + name.toLowerCase());
-}
-
-export function testNavTo(name) {
-  test(`Navigate to: ` + name, async t => {
-    await navTo(t, name, { openHamburger: true });
-  });
-}
-
-export const reloadBetweenNavTests = true;
-
-export const navToNames = "Meetings People Topics Map About Profile".split(" "); // Profile can cause native dialog (unsaved), so should be last
-console.log("navToNames", navToNames);
-
-// TODO configure testcafe to run test from differents files
-
-if (reloadBetweenNavTests) {
-  navToNames.forEach(navToName => {
-    testNavTo(navToName);
-  });
-} else {
-  test(`Navigate to pages`, async t => {
-    await t.click("#menuButtonHamburger");
-    console.log("navToNames", navToNames);
-    for (const navToName of navToNames) {
-      await navTo(t, navToName, { openHamburger: false });
-    }
-  });
-}
+navToPagesTests();
+userProfileTest();
---
+++ NMWeb/e2e-testcafe/testSrc/navigation.testc.ts
@@ -0,0 +1,45 @@
+/** DRY CoC */
+import { getLocation } from './utilsGlobal/utils'
+
+export async function navTo(t, name, { openHamburger }) {
+  // await tz
+  // .maximizeWindow( )
+  console.log("navTo", name);
+  if (openHamburger) {
+    await t.click("#menuButtonHamburger");
+  }
+  await t
+    .click("#navTo" + name)
+    .expect(getLocation())
+    .contains("/" + name.toLowerCase());
+}
+
+export function testNavTo(name) {
+  test(`Navigate to: ` + name, async t => {
+    await navTo(t, name, { openHamburger: true });
+  });
+}
+
+export const navToNames = "Meetings People Topics Map About Profile".split(" "); // Profile can cause native dialog (unsaved), so should be last
+console.log("navToNames", navToNames);
+
+export function navToPages(options: {reloadBetweenNavTests: boolean}) {
+  if (options.reloadBetweenNavTests) {
+    navToNames.forEach(navToName => {
+      testNavTo(navToName);
+    });
+  } else {
+    test(`Navigate to pages without reloading between them (faster and can uncover certain problems)`, async t => {
+      await t.click("#menuButtonHamburger");
+      console.log("navToNames", navToNames);
+      for (const navToName of navToNames) {
+        await navTo(t, navToName, { openHamburger: false });
+      }
+    });
+  }
+}
+
+export function navToPagesTests() {
+  navToPages({reloadBetweenNavTests: false}) // faster first
+  navToPages({reloadBetweenNavTests: true})
+}
